03bd0998be918d54f87b949dad13a1f40756e9cf
Jack <uzjip@student.kit.edu>
2024-04-07 20:45:01 +0200

    aaaaaaaaa

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!--checkboxes and classes to toggle and check-->
+    <div style="float: left; width: 20%;">
+      <div v-for="classGroup in classes" :key="classGroup.name" style="margin-bottom: 1%;">
+        <input type="checkbox" :checked="isSelectedClass(classGroup)" @change.stop="selectClass(classGroup)">
+        <span class="clickable" @click="toggleClassSelection(classGroup)">{{ classGroup.name }}</span>
+      </div>
+    </div>
+    <!--show all students available belongs to the classes checked-->
+    <div style="float: left; width: 30%; border: 1px solid blue; padding: 4%;">
+      <div v-for="classGroup in selectedClasses" :key="classGroup.name">
+        <h3>Students of {{ classGroup.name }}</h3>
+        <ul>
+          <li v-for="student in classGroup.students" :key="student.id" class="clickable"
+            @click="toggleStudentSelection(student)">
+            {{ student.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--show all students selected from the classes-->
+    <div style="float: right; width: 40%; border: 1px solid blue; margin-left: 20px;">
+      <h2>Selected Students</h2>
+      <div style="border: 1px solid black; margin-top: 10px;">
+        <ul>
+          <li v-for="student in selectedStudents" :key="student.id"
+            :class="{ studentSelected: student, clickable: student }">
+            {{ student.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <button class="exma" @click="exmaStudent">EXMA!</button>
+
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { classes } from './classesData.js';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      classes,
+      selectedClasses: [],
+      selectedStudents: []
+    };
+  },
+  computed: {
+    
+  },
+  methods: {
+    selectClass(classGroup) {
+      const index = this.selectedClasses.indexOf(classGroup);
+      if (index > -1) {
+        this.selectedClasses.splice(index, 1);
+      }
+      else {
+        this.selectedClasses.push(classGroup);
+      }
+    },
+    isSelectedClass(classGroup) {
+      return this.selectedClasses.includes(classGroup);
+    },
+    toggleStudentSelection(student) {
+      const index = this.selectedStudents.findIndex(s => s.id === student.id);
+      if (index === -1) {
+        this.selectedStudents.push(student);
+      }
+      else {
+        this.selectedStudents.splice(index, 1);
+      }
+    },
+    toggleClassSelection(classGroup) {
+      classGroup.students.forEach(student => {
+        this.toggleStudentSelection(student);
+      });
+    },
+    exmaStudent() {
+       const namesOfSelectedClasses = this.selectedClasses.map(c => c.name);
+
+      const updatedClasses = this.classes.map(classGroup => ({
+        ...classGroup,
+        students: classGroup.students.filter(student =>
+          !this.selectedStudents.some(selected => selected.id === student.id)
+        )
+      }));
+      this.classes = updatedClasses;
+      const updatedSelectedClasses = this.selectedClasses.slice();
+
+      this.selectedStudents = [];
+      this.selectedClasses = this.classes.filter(classGroup =>
+    namesOfSelectedClasses.includes(classGroup.name)
+  );
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.clickable {
+  cursor: pointer;
+  user-select: none;
+}
+
+.clickable:hover {
+  background-color: #f0f0f0;
+}
+
+.exma {
+  position: absolute;
+  right: 20%;
+  bottom: 20%;
+  padding: 5%;
+
+}
+
+.studentSelected {
+  color: green;
 }
 </style>
---
+++ src/classesData.js
@@ -0,0 +1,52 @@
+export const classes = [
+            {
+                name: 'Class 1',
+                students: [
+                    { id: 1, name: 'Alice', description: 'Enthusiastic about learning.' },
+                    { id: 2, name: 'Bob', description: 'Loves science experiments.' },
+                    { id: 3, name: 'Cathy', description: 'Enjoys reading and literature.' },
+                    { id: 4, name: 'David', description: 'Passionate about mathematics.' },
+                    { id: 5, name: 'Eleanor', description: 'Interested in history.' }
+                ]
+            },
+            {
+                name: 'Class 2',
+                students: [
+                    { id: 6, name: 'Fiona', description: 'Loves sports and teamwork.' },
+                    { id: 7, name: 'George', description: 'Enjoys painting and arts.' },
+                    { id: 8, name: 'Hannah', description: 'Likes to solve puzzles.' },
+                    { id: 9, name: 'Ian', description: 'Fascinated by computers.' },
+                    { id: 10, name: 'Jill', description: 'Engaged in social studies.' }
+                ]
+            },
+            {
+                name: 'Class 3',
+                students: [
+                    { id: 11, name: 'Kevin', description: 'Excels in creative writing.' },
+                    { id: 12, name: 'Linda', description: 'Passionate about environmental science.' },
+                    { id: 13, name: 'Mike', description: 'Avid chess player.' },
+                    { id: 14, name: 'Nora', description: 'Interested in economics.' },
+                    { id: 15, name: 'Oscar', description: 'Enthusiastic about robotics.' }
+                ]
+            },
+            {
+                name: 'Class 4',
+                students: [
+                    { id: 16, name: 'Patty', description: 'Loves to explore cultures.' },
+                    { id: 17, name: 'Quentin', description: 'Enjoys learning languages.' },
+                    { id: 18, name: 'Rachel', description: 'Passionate about music.' },
+                    { id: 19, name: 'Steve', description: 'Excels in physical education.' },
+                    { id: 20, name: 'Tina', description: 'Fascinated by astronomy.' }
+                ]
+            },
+            {
+                name: 'Class 5',
+                students: [
+                    { id: 21, name: 'Uma', description: 'Enjoys creative arts and crafts.' },
+                    { id: 22, name: 'Victor', description: 'Loves to study geography.' },
+                    { id: 23, name: 'Wendy', description: 'Interested in gardening and botany.' },
+                    { id: 24, name: 'Xavier', description: 'Avid video game enthusiast.' },
+                    { id: 25, name: 'Yolanda', description: 'Engaged in volunteer work.' }
+                ]
+            }
+        ]
